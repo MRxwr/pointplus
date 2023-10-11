358b34e2e2c907887e1dfd5a912e3b238e2851cd
MRxwr <nasserhatab@gmail.com>
2023-10-11 08:24:35 +0300

    Update home.php

--- app/request/home.php
+++ app/request/home.php
@@ -101,7 +101,7 @@ if ( isset($rounds[0]["round"]) && !empty($rounds[0]["round"]) && $matches = sel
 				"points" => (string)$prediction[0]["points"]
 			);
 			if( $matches[$i]["isActive"] == 0 ){
-				$points = $prediction[0]["points"];
+				$points = 0;
 				// match result points
 				if( $matches[$i]["goals1"] == $prediction[0]["goals1"] && $matches[$i]["goals2"] == $prediction[0]["goals2"] ){
 					$points = $points + 5;
