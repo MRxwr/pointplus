ca76e67bd94ca0ff2abcc8240d06d5d67bb58e70
MRxwr <nasserhatab@gmail.com>
2023-12-20 02:13:31 +0300

    Update users.php

--- app/admin/pages/users.php
+++ app/admin/pages/users.php
@@ -423,7 +423,7 @@ while ( $row = $result->fetch_assoc() ){
 					WHERE
 					p.status = '{$status[$i]}'
 					AND 
-					p.id = '{$_GET['userId']}'
+					p.userId = '{$_GET['userId']}'
 					ORDER BY m.id ASC
 					";
 			$result = $dbconnect->query($sql);
