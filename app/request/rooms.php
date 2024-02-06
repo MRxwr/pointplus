f0c118aee17e9fd8f5ad0eb2a49cd49e4db6bc7b
MRxwr <nasserhatab@gmail.com>
2024-02-07 02:43:50 +0300

    Update rooms.php

--- app/request/rooms.php
+++ app/request/rooms.php
@@ -46,9 +46,7 @@ if( isset($_POST["join"]) AND !empty($_POST["join"]) ){
 
         if( $room = selectDB("quiz_room","`type` = '1' AND `status` = '0' AND `hidden` = '0'") ){
             $listOfUsers = json_decode($room[0]["listOfUsers"],true);
-            array_push($listOfUsers,array(
-                "id" => $_POST["userId"],
-            ));
+            array_push($listOfUsers,$_POST["userId"]);
             updateDB("quiz_room",array("listOfUsers"=>json_encode($listOfUsers)),"`id` = '{$room[0]["id"]}'");
             $room = selectDB("quiz_room","`type` = '1' AND `status` = '0' AND `hidden` = '0' AND `id` = '{$room[0]["id"]}'");
             $response["room"] = array(
