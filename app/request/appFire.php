fc74ca49fbad8b881f1c47f9ca6c811ee3502a2f
MRxwr <nasserhatab@gmail.com>
2024-02-07 01:56:13 +0300

    Update appFire.php

--- app/request/appFire.php
+++ app/request/appFire.php
@@ -2,11 +2,11 @@
 if( !isset($_POST["userId"]) || empty($_POST["userId"]) ){
     $response["msg"] = "Please set user id";
     echo outputError($response);die();
-}elseif( !isset($_POST["friebase"]) || empty($_POST["friebase"]) ){
-    $response["msg"] = "Please friebase token";
+}elseif( !isset($_POST["firebase"]) || empty($_POST["firebase"]) ){
+    $response["msg"] = "Please firebase token";
     echo outputError($response);die();
 }else{
-    updateDB("users",array("friebase"=>$_POST["friebase"]),"`id` = '{$_POST["userId"]}'");
+    updateDB("users",array("firebase"=>$_POST["firebase"]),"`id` = '{$_POST["userId"]}'");
 }
 
 ?>
