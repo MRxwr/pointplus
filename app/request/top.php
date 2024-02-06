6257e7910cec2b8465a40c553483e0f4b17c38a4
MRxwr <nasserhatab@gmail.com>
2024-02-07 00:07:21 +0300

    Update top.php

--- app/request/top.php
+++ app/request/top.php
@@ -22,7 +22,12 @@ if( isset($_GET["topId"]) && !empty($_GET["topId"]) ){
             "list" => json_decode($top[0]["topUsers"],true)
         );
     }else{
-        $response["top"] = array();
+        $response["top"] = array(
+            "id" => $top[0]["id"],
+            "enTitle" => $top[0]["enTitle"],
+            "arTitle" => $top[0]["arTitle"],
+            "list" => json_decode("",true)
+        );
     }
 }else{
 	if ( $top = selectDataDB("*",'tops',"`status` = '0' AND `hidden` = '0' ORDER BY `id` DESC LIMIT 1") ){
