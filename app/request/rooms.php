594a656335045dc14c3b45697852d719c575b9a6
MRxwr <nasserhatab@gmail.com>
2024-02-07 03:07:06 +0300

    Update rooms.php

--- app/request/rooms.php
+++ app/request/rooms.php
@@ -79,8 +79,8 @@ if( isset($_POST["join"]) AND !empty($_POST["join"]) ){
             );
             insertDB("quiz_room",$dataInsert);
             $room = selectDB("quiz_room",'`type` = "1" AND `status` = "0" AND `hidden` = "0" AND JSON_CONTAINS(listOfUsers, "{"id": "'.$_POST["userId"].'"}")');
+            echo '`type` = "1" AND `status` = "0" AND `hidden` = "0" AND JSON_CONTAINS(listOfUsers, "{"id": "'.$_POST["userId"].'"}")';
             $response["room"] = array(
-                "text" => '`type` = "1" AND `status` = "0" AND `hidden` = "0" AND JSON_CONTAINS(listOfUsers, "{"id": "'.$_POST["userId"].'"}")',
                 "id" => $room[0]["id"],
                 "code" => $room[0]["code"],
                 "listOfUsers" => json_decode($room[0]["listOfUsers"],true),
