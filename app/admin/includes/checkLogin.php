4b7db1a1e5c7ef07361d768a3d0c5222365a1ea7
MRxwr <nasserhatab@gmail.com>
2024-04-19 01:02:04 +0300

    Update checkLogin.php

--- app/admin/includes/checkLogin.php
+++ app/admin/includes/checkLogin.php
@@ -3,7 +3,7 @@ if ( !isset($_COOKIE["createSystem"]) || ( isset( $_GET["page"] ) && $_GET["page
 	setcookie("createSystem", "", time() - 3600, '/');
 	header('LOCATION: pages/logout.php');die();
 }else{
-	if ( $user = selectDB("user"," `cookie` LIKE '{$_COOKIE["createSystem"]}' AND `status` LIKE '0'") ){
+	if ( $user = selectDBUpdated("user"," `cookie` LIKE '{$_COOKIE["createSystem"]}' AND `status` LIKE '0'") ){
 		$userId = $user[0]["id"];
 		$username = $user[0]["username"];
 		$userType = $user[0]["type"];
