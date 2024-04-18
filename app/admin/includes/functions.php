9cb8fb290d0771dce8c891e1857a80e892a25251
MRxwr <nasserhatab@gmail.com>
2024-04-19 01:00:39 +0300

    jupdate login

--- app/admin/includes/functions.php
+++ app/admin/includes/functions.php
@@ -55,6 +55,32 @@ function selectDB($table, $where){
 	}
 }
 
+function selectDBUpdated($table, $where){
+    GLOBAL $dbconnect;
+    $check = [';', '"'];
+    $where = str_replace($check, "", $where);
+    $sql = "SELECT * FROM `{$table}`";
+    if (!empty($where)) {
+        $sql .= " WHERE {$where}";
+    }
+    if ($stmt = $dbconnect->prepare($sql)) {
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $array = array();
+        while ($row = $result->fetch_assoc()) {
+            $array[] = $row;
+        }
+        if (isset($array) && is_array($array)) {
+            return $array;
+        } else {
+            return 0;
+        }
+    } else {
+        $error = array("msg" => "select table error");
+        return outputError($error);
+    }
+}
+
 function selectJoinDB($table, $joinData, $where){
 	GLOBAL $dbconnect;
 	GLOBAL $date;
