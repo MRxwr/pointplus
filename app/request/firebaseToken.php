97af3f14f11934e14c1fc890fb493778248403e9
MRxwr <nasserhatab@gmail.com>
2024-09-18 02:01:24 +0300

    Update firebaseToken.php

--- app/request/firebaseToken.php
+++ app/request/firebaseToken.php
@@ -4,7 +4,7 @@ require "../../vendor/autoload.php";
 use Kreait\Firebase\Factory;
 
 $factory = (new Factory)->withServiceAccount('/path/to/firebase_credentials.json');
-
+ 
 $token = $factory->createAuth();
 
 echo outputData($token);
