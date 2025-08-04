bc294d6f48f2469ab8fc8cec830798efe728969f
MRxwr <nasserhatab@gmail.com>
2025-08-05 00:54:59 +0300

    save

--- app/adminTest/pages/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-setcookie("createSystem", "", time() - 3600, '/');
-setcookie("createSystem", "", time() - 3600, '/');
-header('LOCATION: ../login.php');
-?>
--- app/requestTest1/item.php
+++
@@ -1,9 +0,0 @@
-<?php
-if( $items = selectDataDB("`id`,`arTitle`,`enTitle`,`image`,`coins`","products","`status` = '0' AND `quantity` > '0'") ){
-	$response["items"] = $items;
-	echo outputData($response);
-}else{
-	$response["msg"] = "No items found";
-	echo outputError($response);die();
-}
-?>
