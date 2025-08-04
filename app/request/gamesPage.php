4f65f8579cd2717b4a877f2c2085f0c872990a1c
MRxwr <nasserhatab@gmail.com>
2025-08-04 19:08:42 +0300

    Update gamesPage.php

--- app/request/gamesPage.php
+++ app/request/gamesPage.php
@@ -3,7 +3,7 @@ if( $user = selectDB2('sum(total) as totalPoints','quiz_room', "`winner` LIKE '{
 	$response = array(
         "totalPoints" => $user[0]["totalPoints"] ? $user[0]["totalPoints"] : "0"
     );
-	echo outputData($response);
+	echo outputData($response); 
 }else{
 	$response = array(
         "msg"=>"No user found with this ID.",
